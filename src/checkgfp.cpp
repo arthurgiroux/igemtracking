ae654c5e1265aa74f4b054b353aff5d03982ee1a
Arthur Giroux <arthur.giroux@epfl.ch>
2014-10-18 03:29:50 +0200

    latest version

--- src/checkgfp.cpp
+++ src/checkgfp.cpp
@@ -3,23 +3,80 @@
 
 using namespace cv;
 
+Mat hsv;
+
+void onMouse(int event, int x, int y, int flag, void* param)
+{
+    if (event == EVENT_LBUTTONDOWN) {
+        /*To access each pixel in the images we are using this syntax: image.at(y,x)[c] where y is the row,
+          x is the column and c is H, S or V (0, 1 or 2)*/
+        Vec3b p = hsv.at<Vec3b>(y, x);
+
+        std::cout << "x : " << x << " y : " << y << std::endl;
+        std::cout << "H=" << (int) p.val[0] << ", S=" << (int) p.val[1] << ", V=" << (int) p.val[2]; //p[0] - H, p[1] - S, p[2] - V
+    }
+}
+
 int main(int argc, char** argv)
 {
-    Mat src = imread(argv[1], 1);
-    if (!src.data)
+
+    VideoCapture inputVideo(argv[1]);
+
+    if (!inputVideo.isOpened())
     {
+        std::cout  << "Could not open the input video " << std::endl;
         return -1;
     }
 
-    Mat hsv, green;
-    cvtColor(src, hsv, CV_BGR2HSV);
-    imshow("gfp", hsv);
-    inRange(hsv, Scalar(50, 0, 0), Scalar(70, 255, 255), green);
+    Mat src;
 
-    imshow("green", green);
+    int i;
+    while (true) //Show the image captured in the window and repeat
+    {
+        inputVideo >> src;
+        if (src.empty()) break;
 
- 
-    waitKey(0);
+        std::cout << "frame " << i << std::endl;
+
+        Mat green;
+        cvtColor(src, hsv, CV_BGR2HSV);
+        imshow("src", src);
+
+        // get the range in hsv corresponding to the color that we want to track
+        inRange(hsv, Scalar(85, 140, 0), Scalar(95, 255, 255), green);
+
+        //morphologyEx(green, green, MORPH_OPEN, Mat());
+        //morphologyEx(green, green, MORPH_CLOSE, Mat());
+
+        medianBlur(green, green, 7);
+
+        //imshow("green", green);
+
+        setMouseCallback("src", onMouse, 0);
+
+        cvtColor(green, green, CV_GRAY2BGR);
+
+        Mat onlygreen = src & green;
+     
+        //imshow("onlygreen", onlygreen);
+
+        std::stringstream ss;
+
+
+        // write the images
+        ss << "images/frame_" << i << ".png";
+
+
+        // Extract only the chambers that we want to look at
+        Mat roi(onlygreen, Rect(1020,254,105,210));
+
+        imwrite(ss.str(), roi);
+
+        i++;
+
+    }
+
+    std::cout << "Finished writing" << std::endl;
 
     return 0;
 }
--- src/tracking.cpp
+++ src/tracking.cpp
@@ -3,6 +3,15 @@
 
 using namespace cv;
 
+
+
+// Compute the distance between two 2D points
+double distanceBetweenPoints(Point2f p1, Point2f p2) {
+    // sqrt((x1 - x2)^2 + (y1 - y2)^2)
+    return sqrt((p1.x-p2.x)*(p1.x-p2.x) + (p1.y - p2.y)*(p1.y - p2.y));
+}
+
+
 int main(int argc, char** argv)
 {
     // open the default camera
@@ -15,6 +24,9 @@ int main(int argc, char** argv)
     Mat light, frame, mask;
     namedWindow("tracking", 1);
     int key;
+
+    Point lastPoint; // latest point tracked
+    int i = 0; // number of frames
     while (key != 'q' && key != 'Q')
     {
          // get a new frame from camera
@@ -26,27 +38,56 @@ int main(int argc, char** argv)
         // Threshold the image given the light intensity
         inRange(light, Scalar(200, 0, 0), Scalar(255, 255, 255), mask);
 
-        /// Reduce the noise for circle detection
+        // Reduce the noise for circle detection
         GaussianBlur(mask, mask, Size(15, 15), 5, 5);
 
         vector<Vec3f> circles;
 
-        /// Apply the Hough Transform to find the circles
+        // Apply the Hough Transform to find the circles
         HoughCircles(mask, circles, CV_HOUGH_GRADIENT, 2, mask.rows/8, 30, 50, 0, 0 );
         
 
-        /// Draw the circles detected
-        for( size_t i = 0; i < circles.size(); i++ )
+        // Get the closest circle to the previous one
+        Vec3f goodCircle;
+        double mindistance = -1;
+
+        for (size_t i = 0; i < circles.size(); i++)
         {
             Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
             int radius = cvRound(circles[i][2]);
+
+            double distance = distanceBetweenPoints(center, lastPoint);
+
+            if (mindistance == -1 || distance < mindistance) {
+                std::cout << "new point" << std::endl;
+                lastPoint = center;
+                mindistance = distance;
+                goodCircle = circles[i];
+            }
+        }
+
+        if (circles.size() > 0) {
+
+            std::cout << "drawing circle" << std::endl;
             // circle outline
+            Point center(cvRound(goodCircle[0]), cvRound(goodCircle[1]));
+            int radius = cvRound(goodCircle[2]);
+
             circle(frame, center, radius, Scalar(0,0,255), 3, 8, 0);
         }
 
+
+        std::stringstream ss;
+
+        // write the image
+        ss << "imagestracking/frame_" << i << ".png";
+
+        imwrite(ss.str(), frame);
         imshow("tracking", frame);
 
         key = waitKey(30);
+
+        i++;
     }
 
     return 0;
